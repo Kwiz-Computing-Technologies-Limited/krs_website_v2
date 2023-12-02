ab64778c2e38a37e296e8fd12bf33339d011450e
jean_KRS <jeankwizera@kwizresearch.com>
2023-12-02 17:40:10 +0300

    using gifs for animated charts

--- pages/functions/landingpage_plotly.R
+++ pages/functions/landingpage_plotly.R
@@ -1,39 +1,34 @@
 # Load the bslib, plotly, and htmltools packages
 library(bslib)
+library(ggplot2)
 library(plotly)
 library(htmltools)
 library(htmlwidgets)
 library(later)
+library(gganimate)
 
 # Create an animated scatterplot of mpg vs. hp, grouped by cyl
-landingpage_plotly <- function() {
-  plot_ly(
-    data = mtcars |>
-      mutate(wt = round(wt, 0) * 5,
-             disp = round(disp, 0)),
-    x = ~hp,
-    y = ~mpg,
-    color = ~factor(disp),
-    frame = ~gear,
-    type = "scatter",
-    mode = "markers",
-    marker = list(size = ~(wt))
-  ) |>
-    layout(
-      legend = list(title = list(text = "<b>Displacement</b>")),
-      xaxis = list(title = "HP"),
-      yaxis = list(title = "MPG")
-    ) |> 
-    animation_opts(
-      1200, easing = "circle", redraw = TRUE,
-      mode = "afterall"
-    ) |>
+landingpage_plotly <- function(name) {
+  if(! paste0(name,".gif") %in% list.files("pages/images/gifs/")) {
+    animated_plot = {ggplot(
+      data = mtcars |>
+        mutate(wt = round(wt, 0) * 5,
+               disp = round(disp, 0),
+               gear = factor(gear)),
+      aes(x = hp, y = mpg, color = gear)) +
+      geom_point() + theme_bw() +
+        theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+              panel.background = element_blank(), axis.line = element_blank()) +
+      transition_states(cyl, transition_length = 2, state_length = 1) +
+      enter_grow() + exit_fly() + labs(title = "Cyl: {closest_state}")} |>
+      animate()
     
-    animation_button(visible = TRUE) |>
-    onRender("
-        function(el,x) {
-          Plotly.animate(el);
-        }")
+    anim_save(paste0("pages/images/gifs/",name,".gif"))
+    src = paste0("pages/images/gifs/",name,".gif")
+  }
+  
+  src = paste0("pages/images/gifs/",name,".gif")
+  return(src)
 }
 
 # JS code
